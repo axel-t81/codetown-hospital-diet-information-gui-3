8f8118157a8df79530e543444b424e85822d0e66
Axel <63882492+axel-t81@users.noreply.github.com>
2024-09-18 00:45:10 +1000

    Update gui_template.py
    Solved resizing (via more UNE lectures and a more acute question searched on StackOverflow)

--- gui_template.py
+++ gui_template.py
@@ -7,35 +7,22 @@ def nutrition():
      pass
 
 root = Tk()
-# root.geometry("650x250+400+300")
 
 root.title("Codetown Hospital: Diet Information")
 
-frame = ttk.Frame(root, padding = "3 3 12 12")
+frame = ttk.Frame(root, padding = "3 3 12 12", borderwidth = 5, relief = "sunken")
+
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
 
 frame.grid(column = 0, row = 0, sticky = (N, W, E, S))
 
-rows = 8
-columns = 10
+for i in range(1, 10):
+    frame.rowconfigure(i, weight=1)
 
-for i in range(rows):
-    root.rowconfigure(index=i,weight=1)
-
-for i in range(columns):
-    root.columnconfigure(index=i, weight=1)
-
-
-
-
-# Blank cell create
-#blank_cell = ttk.Label(frame, text = "")
-#blank_cell.grid(column = 1, row = 1, sticky = (N, S, E, W))
-#blank_cell.grid(column = 1, row = 2, sticky = (N, S, E, W))
-#blank_cell.grid(column = 1, row = 3, sticky = (N, S, E, W))
-#blank_cell.grid(column = 1, row = 4, sticky = (N, S, E, W))
-#blank_cell.grid(column = 1, row = 5, sticky = (N, S, E, W))
-#blank_cell.grid(column = 1, row = 6, sticky = (N, S, E, W))
-#blank_cell.grid(column = 1, row = 7, sticky = (N, S, E, W))
+for i in range(1, 6):
+    frame.columnconfigure(i, weight=1)
+       
 
 # Create the labels
 selected_diet_label = ttk.Label(frame, text = "Selected Diet:")
@@ -46,19 +33,13 @@ kilojoules_label = ttk.Label(frame, text = "Kilojoules (kJs):")
 
 select_diet_label = ttk.Label(frame, text = "Select Below the Diet Option to Display:")
 # Display the label
-selected_diet_label.grid(column = 2, row = 2, sticky = E)
-protein_label.grid(column = 2, row = 3, sticky = E)
-carbohydrates_label.grid(column = 2, row = 4, sticky = E)
-fat_label.grid(column = 2, row = 5, sticky = E)
-kilojoules_label.grid(column = 2, row = 6, sticky = E)
-select_diet_label.grid(column = 2, row = 8, columnspan = 3, sticky = (N, S))
+selected_diet_label.grid(column = 2, row = 2, sticky = (N, S, E, W))
+protein_label.grid(column = 2, row = 3, sticky = (N, S, E, W))
+carbohydrates_label.grid(column = 2, row = 4, sticky = (N, S, E, W))
+fat_label.grid(column = 2, row = 5, sticky = (N, S, E, W))
+kilojoules_label.grid(column = 2, row = 6, sticky = (N, S, E, W))
+select_diet_label.grid(column = 2, row = 8, columnspan = 3, sticky = ((N, S, E, W)))
 
-# Create blank label
-blank_label1 = ttk.Label(frame, text = "")
-blank_label2 = ttk.Label(frame, text = "")
-# Display blank labels
-blank_label1.grid(column = 2, row = 1, columnspan = 3, sticky = (N, S))
-blank_label2.grid(column = 2, row = 7, columnspan = 3, sticky = (N, S))
 
 # Create Buttons
 normal_button = ttk.Button(frame, width = 8, text = "Normal", command = nutrition)
@@ -67,22 +48,14 @@ cardiology_button = ttk.Button(frame, width = 8, text = "Cardiology", command = 
 diabetes_button = ttk.Button(frame, width = 8, text = "Diabetes", command = nutrition)	
 kidney_button = ttk.Button(frame, width = 8, text = "Kidney", command = nutrition)		
 # Display Buttons
-normal_button.grid(column = 1, row = 9, sticky = (E, W))
-oncology_button.grid(column = 2, row = 9, sticky = (E, W))
-cardiology_button.grid(column = 3, row = 9, sticky = (E, W))
-diabetes_button.grid(column = 4, row = 9, sticky = (E, W))
-kidney_button.grid(column = 5, row = 9, sticky = (E, W))
+normal_button.grid(column = 1, row = 9, sticky = (N, S, E, W))
+oncology_button.grid(column = 2, row = 9, sticky = (N, S, E, W))
+cardiology_button.grid(column = 3, row = 9, sticky = (N, S, E, W))
+diabetes_button.grid(column = 4, row = 9, sticky = (N, S, E, W))
+kidney_button.grid(column = 5, row = 9, sticky = (N, S, E, W))
 
 for child in frame.winfo_children():			
 	child.grid_configure(padx = 3, pady = 3)
 
-
-### THIS WORKS for HORIZONTAL RESIZING, but not VERTICAL resizing
-#for c in sorted(root.children):
-#    root.children[c].pack()
-### This has been the best guide on both resizing you've found:
-# https://www.youtube.com/watch?v=rZxOe1kVF8Q
-### AND other guides are already in your references
-####### !!!!!!! See Assignment 3 forum where Vera points to how to do this
 
 root.mainloop()
